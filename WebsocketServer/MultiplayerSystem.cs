dcf770ee2bec3512a3028361ce2586cae451170c
Electrostats <Super_justy@hotmail.com>
2023-12-21 01:27:56 -0500

    Added SocketMessage received events

--- WebsocketServer/MultiplayerSystem.cs
+++ WebsocketServer/MultiplayerSystem.cs
@@ -1,19 +1,58 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Concurrent;
+using System.Security.Cryptography.X509Certificates;
 using TootTallyCore.Utils.TootTallyNotifs;
+using TootTallyMultiplayer.APIService;
 using TootTallyWebsocketLibs;
+using WebSocketSharp;
 
 namespace TootTallyMultiplayer
 {
     public class MultiplayerSystem : WebsocketManager
     {
         public Action OnWebSocketOpenCallback;
+        public static JsonConverter[] _dataConverter = new JsonConverter[] { new SocketDataConverter() };
+
+        public ConcurrentQueue<SocketSongInfo> _receivedSongInfo;
+        public ConcurrentQueue<SocketOptionInfo> _receivedSocketOptionInfo;
+
+        public Action<SocketSongInfo> OnSocketSongInfoReceived;
+        public Action<SocketOptionInfo> OnSocketOptionReceived;
 
         public string GetServerID => _id;
 
         public MultiplayerSystem(string serverID, bool isHost) : base(serverID, "wss://spec.toottally.com/mp/join/", "1.0.0")
         {
             ConnectionPending = true;
-            ConnectToWebSocketServer(_url + serverID,TootTallyAccounts.Plugin.GetAPIKey,  isHost);
+            ConnectToWebSocketServer(_url + serverID, TootTallyAccounts.Plugin.GetAPIKey, isHost);
+        }
+
+        public void UpdateStacks()
+        {
+            if (OnSocketSongInfoReceived != null && _receivedSongInfo.TryDequeue(out SocketSongInfo songInfo))
+                OnSocketSongInfoReceived.Invoke(songInfo);
+            if (OnSocketOptionReceived != null && _receivedSocketOptionInfo.TryDequeue(out SocketOptionInfo option))
+                OnSocketOptionReceived.Invoke(option);
+        }
+
+        protected override void OnDataReceived(object sender, MessageEventArgs e)
+        {
+            if (e.IsText)
+            {
+                SocketMessage message;
+                try
+                {
+                    message = JsonConvert.DeserializeObject<SocketMessage>(e.Data, _dataConverter);
+                }
+                catch (Exception) { return; }
+
+                if (message is SocketSongInfo)
+                    _receivedSongInfo.Enqueue(message as  SocketSongInfo);
+                else if (message is SocketOptionInfo)
+                    _receivedSocketOptionInfo.Enqueue(message as SocketOptionInfo);
+            }
         }
 
         protected override void OnWebSocketOpen(object sender, EventArgs e)
@@ -29,6 +68,64 @@ namespace TootTallyMultiplayer
                 TootTallyNotifManager.DisplayNotif($"Disconnected from multiplayer server.");
             if (IsConnected)
                 CloseWebsocket();
+        }
+
+        public enum DataType
+        {
+            UserInfo,
+            SongInfo,
+            LobbyInfo,
+        }
+
+        public enum LobbyOptionType
+        {
+            TitleChanged,
+            PasswordChanged,
+            ModifierChanged,
+            GameSpeedChanged,
+        }
+
+        public class SocketMessage
+        {
+            public string dataType { get; set; }
+        }
+
+        public class SocketOptionInfo : SocketMessage
+        {
+            public LobbyOptionType optionType { get; set; }
+            public object[] values { get; set; }
+        }
+
+        public class SocketSongInfo : SocketMessage
+        {
+            public MultSerializableClasses.MultiplayerSongInfo songInfo { get; set; }
+        }
+
+
+        public class SocketDataConverter : JsonConverter
+        {
+            public override bool CanConvert(Type objectType) => objectType == typeof(SocketMessage);
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                JObject jo = JObject.Load(reader);
+                return Enum.Parse(typeof(DataType), jo["dataType"].Value<string>()) switch
+                {
+                    DataType.SongInfo => jo.ToObject<SocketSongInfo>(serializer),
+                    DataType.LobbyInfo => jo.ToObject<SocketOptionInfo>(serializer),
+                    _ => null,
+                };
+            }
+
+            public override bool CanWrite
+            {
+                get { return false; }
+            }
+
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
